e1db65d0eae235147792f723c139deee4040523d
MarkoJac <47915166+MarkoJac@users.noreply.github.com>
2025-07-31 23:39:18 -0400

    Define result of game and outcome messages
    Win, loss, and draw conditional statements added.

--- javascript.js
+++ javascript.js
@@ -10,15 +10,15 @@ function getComputerChoice() {
    let x = Math.floor(Math.random() * 3 + 1)
    
    if (x == 1) {
-    console.log("Rock");
+    console.log("ROCK");
    }
 
    else if (x == 2) {
-    console.log("Paper");
+    console.log("PAPER");
    }
 
    else {
-    console.log("Scissors");
+    console.log("SCISSORS");
    }
 }
 
@@ -36,3 +36,30 @@ function getHumanChoice() {
  
 let humanScore = 0
 let computerScore = 0
+
+// Write logic to play a single round of the game
+
+// Call the players choices
+// Normalize human input to be case-insensitive
+// Name the winner
+// Increment scores
+
+function playRound(humanChoice, computerChoice) {
+    
+    const win = "You win!"
+    const loss = "Sorry, you lose!"
+    const draw = "It's a draw!"
+
+    let result = (humanChoice == 'ROCK' && computerChoice == 'ROCK') ? draw :
+        (humanChoice == 'ROCK' && computerChoice == 'PAPER') ? loss :
+        (humanChoice == 'ROCK' && computerChoice == 'SCISSORS') ? win :
+        (humanChoice == 'PAPER' && computerChoice == 'ROCK') ? win :
+        (humanChoice == 'PAPER' && computerChoice == 'PAPER') ? draw :
+        (humanChoice == 'PAPER' && computerChoice == 'SCISSORS') ? loss :
+        (humanChoice == 'SCISSORS' && computerChoice == 'ROCK') ? loss :
+        (humanChoice == 'SCISSORS' && computerChoice == 'PAPER') ? win :
+        (humanChoice == 'SCISSORS' && computerChoice == 'SCISSORS') ? draw :
+        
+    console.log(result)
+        
+}
